86d727559cf8ede36af532f7a41474a08923fb1d
Pasha-Livinskiy <pasha_livinskiy@mail.ru>
2022-06-10 18:07:19 +0300

    исправил ошибки. доделал script

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,56 @@
 const popup = document.querySelector('.popup');
 const profileEditButton = document.querySelector('.profile__edit-button'); 
-const closePopupButton = document.querySelector('.close-popup'); 
+const closePopup = document.querySelector('.popup__close'); 
 const inputName = document.querySelectorAll('#input-name');
 const inputJob = document.querySelectorAll('#input-job');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 const popupBody = document.querySelector('.popup__body');
+const popupContainer = document.querySelectorAll('.popup__container');
+const popupButton = document.querySelector('.popup__button');
 
 
-function popupOpen() {
+function popupProfile() {
     if (popup.classList.contains('popup_opened')) {
        (popup.classList.toggle('popup_opened'))
     } else {
         popup.classList.toggle('popup_opened');
         inputName[0].value = profileName.textContent;
         inputJob[0].value = profileDescription.textContent;
+        document.addEventListener ('keyup', closeWithEsc);
     };
 };
 
 function popupClose() {
-    popup.classList.remove('popup_opened')
+    popup.classList.remove('popup_opened');
+    document.removeEventListener ('keyup', closeWithEsc);
 };
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = inputName[0].value;
     profileDescription.textContent = inputJob[0].value;
+    popupClose()
 };
 
-profileEditButton.addEventListener('click', popupOpen);
-closePopupButton.addEventListener('click', popupClose);
+document.addEventListener('click', (e) => { 
+    if(e.target === popup) { 
+        popup.classList.remove('popup_opened'); 
+    }
+});
+
+const closeWithEsc = (evt) => {
+  if (evt.key === 'Escape') {
+    const popupOpened = document.querySelector('.popup_opened');
+    popupClose(popupOpened);
+  }
+}
+
+
+profileEditButton.addEventListener('click', popupProfile);
+closePopup.addEventListener('click', popupClose);
 popupBody.addEventListener('submit', formSubmitHandler);
-popupBody.addEventListener('click', formSubmitHandler);
+
 
 
 
